66ee58054c941881addc4b184e5242ad74ee2428
Chris Brooks <cbrooks@iwsinc.com>
2021-02-03 13:42:53 -0800

    correct the Package.swift file

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "IWSAuthSDKiOS",
-            targets: ["IWSAuthSDKiOS"])
+            targets: ["GMI"])
         
     ],
     dependencies: [
